14a4b57b4b45cb6ed917ac4a42c9ea9c2b569826
harish3105 <a.harish3105@gmail.com>
2024-08-26 11:37:20 +0530

    first commit

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+Assignment Link:  https://drive.google.com/file/d/1RK1bCm993C8lxx5nIcobDQLs-EbVNe4n/view
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Login from "./Components/login";
+import Sidebar from "./Components/sidebar";
+import MainRoutes from "./Components/mainRoutes";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { isAuthenticated } = useAuth0();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+      {isAuthenticated ? (
+        <div className="flex">
+          <Sidebar />
+          <MainRoutes />
+        </div>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 }
